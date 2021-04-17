7a8f98f9e658c088264008c68f3d42931408261a
Luise Pinna <luisepinna@gmail.com>
2021-04-17 09:20:39 -0300

    aula 03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,21 @@ import { AppComponent } from './app.component';
 import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
 import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
 
-//importando as classes para uso da biblioteca de rotas do angular
-//estas rotas serão utilizadas para navegação da SPA - Single Page Application
+// importando as classes para uso da biblioteca de rotas do angular
+// estas rotas serão utilizadas para navegação da SPA - Single Page Application
 import { Routes, RouterModule } from '@angular/router';
 
-//mapear uma rota (URL) para cada componente do projeto..
-const routes : Routes = [
+// importando as classes para uso da biblioteca de formulários do angular
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// importando as classes para uso da biblioteca de acesso à APIs do angular (Http Client)
+import { HttpClientModule } from '@angular/common/http';
+
+// import
+import { NgxPaginationModule } from 'ngx-pagination';
+
+// mapear uma rota (URL) para cada componente do projeto..
+const routes: Routes = [
   { path : 'cadastro-clientes', component : CadastroClientesComponent },
   { path : 'consulta-clientes', component : ConsultaClientesComponent }
 ];
@@ -23,7 +32,11 @@ const routes : Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes) //registrando a configuração de rotas!
+    HttpClientModule, // registrando biblioteca para chamadas de API
+    FormsModule, // registrando biblioteca para formulários
+    ReactiveFormsModule, // registrando biblioteca para formulários
+    NgxPaginationModule, // registrando
+    RouterModule.forRoot(routes) // registrando a configuração de rotas!
   ],
   providers: [],
   bootstrap: [AppComponent]
